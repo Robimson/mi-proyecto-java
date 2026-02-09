8ca1dc791c8b0a5a2f919a9400059503f8d01b1d
Robinson <rcandom@uteq.edu.ec>
2026-02-08 21:40:17 -0500

    Primer commit del proyecto

---
+++ src/main/java/org/uteq/miproyectoejemplo/Main.java
@@ -0,0 +1,7 @@
+package org.uteq.miproyectoejemplo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo practico");
+    }
+}
---
+++ src/main/java/org/uteq/miproyectoejemplo/MiProyectoEjemploApplication.java
@@ -0,0 +1,13 @@
+package org.uteq.miproyectoejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiProyectoEjemploApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiProyectoEjemploApplication.class, args);
+    }
+
+}
